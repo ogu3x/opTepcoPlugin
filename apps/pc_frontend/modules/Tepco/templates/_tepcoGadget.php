564e4344d375db206c3d0eb3a55f45544e018a41
Kimura Youichi <kim.upsilon@bucyou.net>
2011-03-31 20:47:55 +0900

    ガジェットの並び替えに対応

--- apps/pc_frontend/modules/Tepco/templates/_tepcoGadget.php
+++ apps/pc_frontend/modules/Tepco/templates/_tepcoGadget.php
@@ -1,9 +1,11 @@
+<div id="tepcoBox_<?php echo $gadget->id ?>">
 <div id="tepco_img" style="padding:0;margin:2px 0;">
 <table style="padding:0;margin:0;width:270px">
 <tr><td colspan="2" style="padding:0;height:15px;"><td></tr>
 <tr><td id="tepco_info" colspan="2" style="width:200px;padding:2px 0 0 3px;font-family:Helvetica;font-size:11pt;"></td></tr>
 <tr><td style="width:192px;"></td><td id="tepco_rate" style="padding:0;font-size:23px;font-weight:bold;line-height:30px;"></td></tr>
 </table>
+</div>
 </div>
 
 <script type="text/javascript"> 
